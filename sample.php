868b531158ef8d3c8cc26e8daaec950622f4f9ca
Naoki <naoki@alpha-do.com>
2014-12-05 15:30:03 +0900

    Account,Balance正常値でのテストおっけー

---
+++ sample.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__FILE__).'/omise/Omise.php';
+
+$omise = new Omise('skey_test_4y9cewl0rgwji2kbbcb', 'pkey_test_4y9cewl0s1osh44ouud');
+
+// $omiseAccount = $omise->getOmiseAccessAccount()->retrieve();
+// echo("object:".$omiseAccount->getObject()."\n");
+// echo("id:".$omiseAccount->getID()."\n");
+// echo("email:".$omiseAccount->getEmail()."\n");
+// echo("created:".$omiseAccount->getCreated()."\n");
+// var_dump($omiseAccount);
+
+// $omiseBalance = $omise->getOmiseAccessBalance()->retrieve();
+// echo("object:".$omiseBalance->getObject()."\n");
+// echo("livemode:".$omiseBalance->getLivemode()."\n");
+// echo("available:".$omiseBalance->getAvailable()."\n");
+// echo("total:".$omiseBalance->getTotal()."\n");
+// echo("currency:".$omiseBalance->getCurrency()."\n");
+// var_dump($omiseBalance);
