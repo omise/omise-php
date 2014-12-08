22357d9d9224f54c9e26bc587fd9e0c7b6e2d9a0
Naoki <naoki@alpha-do.com>
2014-12-08 11:00:41 +0900

    トークンの再発行の実装

--- omise/access/OmiseAccessTokens.php
+++ omise/access/OmiseAccessTokens.php
@@ -13,7 +13,7 @@ class OmiseAccessTokens extends OmiseAccessBase {
 	const PARAM_CARD_CITY = 'card[city]';
 	
 	/**
-	 * 
+	 * トークンを作成する。引数はCardCreateInfoオブジェクト
 	 * @param OmiseCardCreateInfo $card
 	 * @return OmiseTokens
 	 */
@@ -28,6 +28,17 @@ class OmiseAccessTokens extends OmiseAccessBase {
 			self::PARAM_CARD_CITY => $cardCreateInfo->getCity()
 		);
 		$array = parent::execute(parent::URLBASE_VAULT.'/tokens', parent::REQUEST_POST, $this->_publickey, $param);
+		
+		return new OmiseTokens($array);
+	}
+	
+	/**
+	 * トークンIDに紐づく情報を再取得する
+	 * @param string $tokenID
+	 * @return OmiseTokens
+	 */
+	function retrieve($tokenID) {
+		$array = parent::execute(parent::URLBASE_VAULT.'/tokens/'.$tokenID, parent::REQUEST_GET, $this->_publickey);
 		
 		return new OmiseTokens($array);
 	}
--- sample.php
+++ sample.php
@@ -20,8 +20,31 @@ try {
 // 	echo("currency:".$omiseBalance->getCurrency()."\n");
 // 	var_dump($omiseBalance);
 	
-	$cardCreateInfo = new OmiseCardCreateInfo('Somchai Prasert', '4242424242424242', '10', '2018', '123', '10320', 'Bangkok');
-	$omiseTokens = $omise->getOmiseAccessTokens()->create($cardCreateInfo);
+// 	$cardCreateInfo = new OmiseCardCreateInfo('Somchai Prasert', '4242424242424242', '10', '2018', '123', '10320', 'Bangkok');
+// 	$omiseTokens = $omise->getOmiseAccessTokens()->create($cardCreateInfo);
+// 	echo("object:".$omiseTokens->getObject()."\n");
+// 	echo("id:".$omiseTokens->getID()."\n");
+// 	echo("livemode:".$omiseTokens->getLivemode()."\n");
+// 	echo("location:".$omiseTokens->getLocation()."\n");
+// 	echo("used:".$omiseTokens->getUsed()."\n");
+// 	echo("card[object]:".$omiseTokens->getCard()->getObject()."\n");
+// 	echo("card[id]:".$omiseTokens->getCard()->getID()."\n");
+// 	echo("card[livemode]:".$omiseTokens->getCard()->getLivemode()."\n");
+// 	echo("card[country]:".$omiseTokens->getCard()->getCountry()."\n");
+// 	echo("card[city]:".$omiseTokens->getCard()->getCity()."\n");
+// 	echo("card[postal_code]:".$omiseTokens->getCard()->getPostalCode()."\n");
+// 	echo("card[financing]:".$omiseTokens->getCard()->getFinancing()."\n");
+// 	echo("card[last_digits]:".$omiseTokens->getCard()->getLastDigits()."\n");
+// 	echo("card[brand]:".$omiseTokens->getCard()->getBrand()."\n");
+// 	echo("card[expiration_month]:".$omiseTokens->getCard()->getExpirationMonth()."\n");
+// 	echo("card[expiration_year]:".$omiseTokens->getCard()->getExpirationYear()."\n");
+// 	echo("card[fingerprint]:".$omiseTokens->getCard()->getFingerprint()."\n");
+// 	echo("card[name]:".$omiseTokens->getCard()->getName()."\n");
+// 	echo("card[created]:".$omiseTokens->getCard()->getCreated()."\n");
+// 	echo("created:".$omiseTokens->getCreated()."\n");
+// 	var_dump($omiseTokens);
+
+	$omiseTokens = $omise->getOmiseAccessTokens()->retrieve('tokn_test_4ybagptsw8m0bqogzhk');
 	echo("object:".$omiseTokens->getObject()."\n");
 	echo("id:".$omiseTokens->getID()."\n");
 	echo("livemode:".$omiseTokens->getLivemode()."\n");
@@ -43,6 +66,7 @@ try {
 	echo("card[created]:".$omiseTokens->getCard()->getCreated()."\n");
 	echo("created:".$omiseTokens->getCreated()."\n");
 	var_dump($omiseTokens);
+	
 } catch(OmiseException $e) {
 	var_dump($e);
 }
