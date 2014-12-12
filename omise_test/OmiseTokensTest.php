4b2c18b4a06acfa017c3ff42cb91216d7b43c80e
Naoki <naoki@alpha-do.com>
2014-12-12 09:52:37 +0900

    標準パターンのテストケースretrieveの完了

--- omise_test/OmiseTokensTest.php
+++ omise_test/OmiseTokensTest.php
@@ -40,4 +40,31 @@ class OmiseTokensTest extends OmiseTest {
 		echo("  card['created']:".$omiseTokens->getCard()->getCreated()."\n");
 		echo('created:'.$omiseTokens->getCreated()."\n");
 	}
+	
+	public function retrieve($tokenID) {
+		$omise = new Omise(parent::PUBLICKEY, parent::SECRETKEY);
+		
+		$omiseTokens = $omise->getOmiseAccessTokens()->retrieve($tokenID);
+
+		echo('object:'.$omiseTokens->getObject()."\n");
+		echo('id:'.$omiseTokens->getID()."\n");
+		echo('livemode:'.$omiseTokens->getLivemode()."\n");
+		echo('location:'.$omiseTokens->getLocation()."\n");
+		echo('used:'.$omiseTokens->getUsed()."\n");
+		echo("  card['object']:".$omiseTokens->getCard()->getObject()."\n");
+		echo("  card['id']:".$omiseTokens->getCard()->getID()."\n");
+		echo("  card['livemode']:".$omiseTokens->getCard()->getLivemode()."\n");
+		echo("  card['country']:".$omiseTokens->getCard()->getCountry()."\n");
+		echo("  card['city']:".$omiseTokens->getCard()->getCity()."\n");
+		echo("  card['postal_code']:".$omiseTokens->getCard()->getPostalCode()."\n");
+		echo("  card['financing']:".$omiseTokens->getCard()->getFinancing()."\n");
+		echo("  card['last_digits']:".$omiseTokens->getCard()->getLastDigits()."\n");
+		echo("  card['brand']:".$omiseTokens->getCard()->getBrand()."\n");
+		echo("  card['expiration_month']:".$omiseTokens->getCard()->getExpirationMonth()."\n");
+		echo("  card['expiration_year']:".$omiseTokens->getCard()->getExpirationYear()."\n");
+		echo("  card['fingerprint']:".$omiseTokens->getCard()->getFingerprint()."\n");
+		echo("  card['name']:".$omiseTokens->getCard()->getName()."\n");
+		echo("  card['created']:".$omiseTokens->getCard()->getCreated()."\n");
+		echo('created:'.$omiseTokens->getCreated()."\n");
+	}
 }
--- sample.php
+++ sample.php
@@ -9,9 +9,12 @@ try {
 	
 // 	$balanceTest = new OmiseBalanceTest();
 // 	$balanceTest->retrieve();
-
-	$tokenTest = new OmiseTokensTest();
-	$tokenTest->create();
+	
+	
+// 	$tokenTest = new OmiseTokensTest();
+// 	$tokenTest->create();
+// 	$tokenTest->retrieve('tokn_test_4ycu4exhnpni3yqbo9q');
+	
 	
 } catch(OmiseException $e) {
 	var_dump($e);
