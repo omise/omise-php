b76fa4df536cf080489683d53453eeedf982c9b0
Naoki <naoki@alpha-do.com>
2014-12-22 12:28:10 +0900

    Charges::createの実装

--- lib/Omise.php
+++ lib/Omise.php
@@ -24,10 +24,16 @@ require_once dirname(__FILE__).'/omise/OmiseCharges.php';
 // 		'postal_code' => '10320',
 // 		'security_code' => 123
 // )));
-// var_dump($object);
-
 // $object = OmiseTokens::retrive('tokn_test_4yfpjbu99utp0m6xpp0');
 // var_dump($object);
 
-$object = OmiseCharges::retrive();
-var_dump($object);
+// $object = OmiseCharges::retrive();
+// $object = OmiseCharges::create(array(
+//   'return_uri' => 'https://example.co.th/orders/384/complete',
+//   'amount' => 100000,
+//   'currency' => 'thb',
+//   'description' => 'Order-384',
+//   'ip' => '127.0.0.1',
+//   'card' => 'tokn_test_4yfpjbu99utp0m6xpp0'
+// ));
+// var_dump($object);
--- lib/omise/OmiseCharges.php
+++ lib/omise/OmiseCharges.php
@@ -8,6 +8,10 @@ class OmiseCharges extends OmiseApiResource {
 		return parent::retrive(get_class(), self::getUrl(), $publickey, $secretkey);
 	}
 	
+	public static function create($params, $publickey = null, $secretkey = null) {
+		return parent::create(get_class(), self::getUrl(), $params, $publickey, $secretkey);
+	}
+	
 	private static function getUrl($id = '') {
 		return OMISE_API_URL.self::ENDPOINT.'/'.$id;
 	}
