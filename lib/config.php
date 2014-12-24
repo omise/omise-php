8ceb29ad867fa904564a77e58eade53e7460bd04
Naoki <naoki@alpha-do.com>
2014-12-24 13:01:24 +0900

    configの鍵を消した

--- lib/config.php
+++ lib/config.php
@@ -2,5 +2,5 @@
 define('OMISE_PHP_LIB_VERSION', '1.0.0');
 define('OMISE_API_VERSION', '2014-07-27');
 
-define('OMISE_PUBLIC_KEY', 'pkey_test_4y9cewl0s1osh44ouud');
-define('OMISE_SECRET_KEY', 'skey_test_4y9cewl0rgwji2kbbcb');
+define('OMISE_PUBLIC_KEY', 'input your public key');
+define('OMISE_SECRET_KEY', 'input your secret key');
