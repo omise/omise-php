dfc6aa305cd23fd56ffcfe0aa2952175d1f38eb6
Robin Clart <robin@omise.co>
2015-01-12 09:52:47 +0000

    Don't use config file

--- lib/Omise.php
+++ lib/Omise.php
@@ -1,5 +1,8 @@
 <?php
 
+define('OMISE_PHP_LIB_VERSION', '1.0.0');
+define('OMISE_API_VERSION', '2014-07-27');
+
 require_once dirname(__FILE__).'/omise/OmiseAccount.php';
 require_once dirname(__FILE__).'/omise/OmiseBalance.php';
 require_once dirname(__FILE__).'/omise/OmiseToken.php';
