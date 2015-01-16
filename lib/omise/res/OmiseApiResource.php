719cdc7d0b0a879ec057d7aaa83d12c69c949a8d
Robin Clart <robin@omise.co>
2015-01-16 10:21:07 +0000

    Add ca cert bundle

--- lib/omise/res/OmiseApiResource.php
+++ lib/omise/res/OmiseApiResource.php
@@ -153,16 +153,18 @@ class OmiseApiResource extends OmiseObject {
         // Only follows up to 3 redirections.
         CURLOPT_MAXREDIRS => 3,
         // Track the header request string and set the referer on redirect.
-        CURLINFO_HEADER_OUT=>true,
+        CURLINFO_HEADER_OUT => true,
         CURLOPT_AUTOREFERER => true,
         // Make HTTP error code above 400 an error.
-        //CURLOPT_FAILONERROR => true,
+        // CURLOPT_FAILONERROR => true,
         // Time before the request is aborted.
         CURLOPT_TIMEOUT => $this->OMISE_TIMEOUT,
         // Time before the request is aborted when attempting to connect.
         CURLOPT_CONNECTTIMEOUT => $this->OMISE_CONNECTTIMEOUT,
         // Authentication.
-        CURLOPT_USERPWD => $userpwd
+        CURLOPT_USERPWD => $userpwd,
+        // CA bundle.
+        CURLOPT_CAINFO => dirname(__FILE__).'/../../../data/ca_certificates.pem'
     );
 
     // Also merge POST parameters with the option.
