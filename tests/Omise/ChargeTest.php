8d621838ad58b9b031175d23d6709676f5dd0a78
AD-PC80MAC <naoki@alpha-do.com>
2015-01-28 11:53:40 +0900

    鍵消し忘れていたので消した・・危ない

--- tests/Omise/ChargeTest.php
+++ tests/Omise/ChargeTest.php
@@ -4,8 +4,8 @@ namespace Omise\Tests;
 
 require_once dirname(__FILE__).'/../../vendor/autoload.php';
 
-define('OMISE_PUBLIC_KEY', 'pkey_test_4y9cewl0s1osh44ouud');
-define('OMISE_SECRET_KEY', 'skey_test_4y9cewl0rgwji2kbbcb');
+define('OMISE_PUBLIC_KEY', 'pkey');
+define('OMISE_SECRET_KEY', 'skey');
 
 class ChargeTest extends \PHPUnit_Framework_TestCase {
   static $_charge;
