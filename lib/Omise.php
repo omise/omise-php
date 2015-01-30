54ef6cf93cf81420c1447a94cf7d047323efe888
AD-PC80MAC <naoki@alpha-do.com>
2015-01-30 17:40:47 +0900

    require用ファイルに新クラスの追加

--- lib/Omise.php
+++ lib/Omise.php
@@ -3,8 +3,11 @@
 require_once dirname(__FILE__).'/omise/OmiseAccount.php';
 require_once dirname(__FILE__).'/omise/OmiseBalance.php';
 require_once dirname(__FILE__).'/omise/OmiseCard.php';
+require_once dirname(__FILE__).'/omise/OmiseCardList.php';
 require_once dirname(__FILE__).'/omise/OmiseToken.php';
 require_once dirname(__FILE__).'/omise/OmiseCharge.php';
 require_once dirname(__FILE__).'/omise/OmiseCustomer.php';
+require_once dirname(__FILE__).'/omise/OmiseRefund.php';
+require_once dirname(__FILE__).'/omise/OmiseRefundList.php';
 require_once dirname(__FILE__).'/omise/OmiseTransfer.php';
 require_once dirname(__FILE__).'/omise/OmiseTransaction.php';
