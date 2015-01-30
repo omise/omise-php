571079da7e3b747df5b76edcad94cc1f2c91415b
AD-PC80MAC <naoki@alpha-do.com>
2015-01-30 15:00:22 +0900

    OmiseCardのrequireが消えちゃってたから戻し

--- lib/Omise.php
+++ lib/Omise.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__).'/omise/OmiseAccount.php';
 require_once dirname(__FILE__).'/omise/OmiseBalance.php';
+require_once dirname(__FILE__).'/omise/OmiseCard.php';
 require_once dirname(__FILE__).'/omise/OmiseToken.php';
 require_once dirname(__FILE__).'/omise/OmiseCharge.php';
 require_once dirname(__FILE__).'/omise/OmiseCustomer.php';
