de3713d666034424f9f9a4aeeee0027270952663
Robin Clart <robin@omise.co>
2015-01-30 12:31:24 +0700

    Remove require refund

--- lib/Omise.php
+++ lib/Omise.php
@@ -5,6 +5,5 @@ require_once dirname(__FILE__).'/omise/OmiseBalance.php';
 require_once dirname(__FILE__).'/omise/OmiseToken.php';
 require_once dirname(__FILE__).'/omise/OmiseCharge.php';
 require_once dirname(__FILE__).'/omise/OmiseCustomer.php';
-require_once dirname(__FILE__).'/omise/OmiseRefund.php';
 require_once dirname(__FILE__).'/omise/OmiseTransfer.php';
 require_once dirname(__FILE__).'/omise/OmiseTransaction.php';
