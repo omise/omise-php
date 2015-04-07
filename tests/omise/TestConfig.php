dc8ec03e7b52364ff0198ac0f2657cc2ebbea021
guzzilar <iam.nuttanon@gmail.com>
2015-04-07 23:21:43 +0700

    Added public & secret key into test config, added phpunit file (for CircleCI's bot)

--- tests/omise/TestConfig.php
+++ tests/omise/TestConfig.php
@@ -2,5 +2,5 @@
 
 define("LIB_PATH", dirname(__FILE__).'/../../lib');
 
-define('OMISE_PUBLIC_KEY', 'pkey');
-define('OMISE_SECRET_KEY', 'skey');
+define('OMISE_PUBLIC_KEY', 'pkey_test_4zfuolwdegumf4k2qba');
+define('OMISE_SECRET_KEY', 'skey_test_4zfuom5ugbndd4z8nlf');
