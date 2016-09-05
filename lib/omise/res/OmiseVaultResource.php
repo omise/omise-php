8036d5874e477f8a99d5f86dea7c34701e29dc9b
guzzilar <iam.nuttanon@gmail.com>
2016-09-05 16:32:43 +0700

    :lipstick: code styling, lib/omise/res/OmiseVaultResource

--- lib/omise/res/OmiseVaultResource.php
+++ lib/omise/res/OmiseVaultResource.php
@@ -2,12 +2,15 @@
 
 require_once dirname(__FILE__).'/OmiseApiResource.php';
 
-class OmiseVaultResource extends OmiseApiResource {
-  /**
-   * Returns the public key.
-   * @return string
-   */
-  protected function getResourceKey() {
-    return $this->_publickey;
-  }
+class OmiseVaultResource extends OmiseApiResource
+{
+    /**
+     * Returns the public key.
+     *
+     * @return string
+     */
+    protected function getResourceKey()
+    {
+        return $this->_publickey;
+    }
 }
