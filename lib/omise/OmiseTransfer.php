b469f685d8e0a63a826603607d93389fab9e349b
guzzilar <iam.nuttanon@gmail.com>
2016-09-05 16:31:58 +0700

    :lipstick: code styling, lib/omise/OmiseTransfer

--- lib/omise/OmiseTransfer.php
+++ lib/omise/OmiseTransfer.php
@@ -2,80 +2,97 @@
 
 require_once dirname(__FILE__).'/res/OmiseApiResource.php';
 
-class OmiseTransfer extends OmiseApiResource {
-  const ENDPOINT = 'transfers';
+class OmiseTransfer extends OmiseApiResource
+{
+    const ENDPOINT = 'transfers';
 
-  /**
-   * Retrieves a transfer.
-   * @param string $id
-   * @param string $publickey
-   * @param string $secretkey
-   * @return OmiseTransfer
-   */
-  public static function retrieve($id = '', $publickey = null, $secretkey = null) {
-    return parent::g_retrieve(get_class(), self::getUrl($id), $publickey, $secretkey);
-  }
-
-  /**
-   * Creates a transfer.
-   * @param unknown $params
-   * @param string $publickey
-   * @param string $secretkey
-   * @return OmiseTransfer
-   */
-  public static function create($params, $publickey = null, $secretkey = null) {
-    return parent::g_create(get_class(), self::getUrl(), $params, $publickey, $secretkey);
-  }
-
-  /**
-   * (non-PHPdoc)
-   * @see OmiseApiResource::g_reload()
-   */
-  public function reload() {
-    if($this['object'] === 'transfers') {
-      parent::g_reload(self::getUrl($this['id']));
-    } else {
-      parent::g_reload(self::getUrl());
+    /**
+     * Retrieves a transfer.
+     *
+     * @param  string $id
+     * @param  string $publickey
+     * @param  string $secretkey
+     *
+     * @return OmiseTransfer
+     */
+    public static function retrieve($id = '', $publickey = null, $secretkey = null)
+    {
+        return parent::g_retrieve(get_class(), self::getUrl($id), $publickey, $secretkey);
     }
-  }
 
-  /**
-   * Updates the transfer amount.
-   */
-  public function save() {
-    $this->update(array('amount' => $this['amount']));
-  }
+    /**
+     * Creates a transfer.
+     *
+     * @param  mixed  $params
+     * @param  string $publickey
+     * @param  string $secretkey
+     *
+     * @return OmiseTransfer
+     */
+    public static function create($params, $publickey = null, $secretkey = null)
+    {
+        return parent::g_create(get_class(), self::getUrl(), $params, $publickey, $secretkey);
+    }
 
-  /**
-   * (non-PHPdoc)
-   * @see OmiseApiResource::g_update()
-   */
-  protected function update($params) {
-    parent::g_update(self::getUrl($this['id']), $params);
-  }
+    /**
+     * (non-PHPdoc)
+     *
+     * @see OmiseApiResource::g_reload()
+     */
+    public function reload()
+    {
+        if ($this['object'] === 'transfers') {
+            parent::g_reload(self::getUrl($this['id']));
+        } else {
+            parent::g_reload(self::getUrl());
+        }
+    }
 
-  /**
-   * (non-PHPdoc)
-   * @see OmiseApiResource::g_destroy()
-   */
-  public function destroy() {
-    parent::g_destroy(self::getUrl($this['id']));
-  }
+    /**
+     * Updates the transfer amount.
+     */
+    public function save()
+    {
+        $this->update(array('amount' => $this['amount']));
+    }
 
-  /**
-   * (non-PHPdoc)
-   * @see OmiseApiResource::isDestroyed()
-   */
-  public function isDestroyed() {
-    return parent::isDestroyed();
-  }
+    /**
+     * (non-PHPdoc)
+     *
+     * @see OmiseApiResource::g_update()
+     */
+    protected function update($params)
+    {
+        parent::g_update(self::getUrl($this['id']), $params);
+    }
 
-  /**
-   *
-   * @param string $id
-   * @return string
-   */
-  private static function getUrl($id = '') {
-    return OMISE_API_URL.self::ENDPOINT.'/'.$id;
-  }
+    /**
+     * (non-PHPdoc)
+     *
+     * @see OmiseApiResource::g_destroy()
+     */
+    public function destroy()
+    {
+        parent::g_destroy(self::getUrl($this['id']));
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see OmiseApiResource::isDestroyed()
+     */
+    public function isDestroyed()
+    {
+        return parent::isDestroyed();
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return string
+     */
+    private static function getUrl($id = '')
+    {
+        return OMISE_API_URL.self::ENDPOINT.'/'.$id;
+    }
 }
