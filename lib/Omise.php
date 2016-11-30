6f621b14f467b10654e94929d619a134d8ac390a
guzzilar <iam.nuttanon@gmail.com>
2016-11-30 18:55:16 +0700

    Update composer & files to require /lib/legacy instead of /lib/omise classes

--- lib/Omise.php
+++ lib/Omise.php
@@ -1,16 +1,15 @@
 <?php
-
-require_once dirname(__FILE__).'/omise/OmiseAccount.php';
-require_once dirname(__FILE__).'/omise/OmiseBalance.php';
-require_once dirname(__FILE__).'/omise/OmiseCard.php';
-require_once dirname(__FILE__).'/omise/OmiseCardList.php';
-require_once dirname(__FILE__).'/omise/OmiseDispute.php';
-require_once dirname(__FILE__).'/omise/OmiseEvent.php';
-require_once dirname(__FILE__).'/omise/OmiseToken.php';
-require_once dirname(__FILE__).'/omise/OmiseCharge.php';
-require_once dirname(__FILE__).'/omise/OmiseCustomer.php';
-require_once dirname(__FILE__).'/omise/OmiseRefund.php';
-require_once dirname(__FILE__).'/omise/OmiseRefundList.php';
-require_once dirname(__FILE__).'/omise/OmiseTransfer.php';
-require_once dirname(__FILE__).'/omise/OmiseTransaction.php';
-require_once dirname(__FILE__).'/omise/OmiseRecipient.php';
+require_once dirname(__FILE__).'/legacy/OmiseAccount.php';
+require_once dirname(__FILE__).'/legacy/OmiseBalance.php';
+require_once dirname(__FILE__).'/legacy/OmiseCard.php';
+require_once dirname(__FILE__).'/legacy/OmiseCardList.php';
+require_once dirname(__FILE__).'/legacy/OmiseDispute.php';
+require_once dirname(__FILE__).'/legacy/OmiseEvent.php';
+require_once dirname(__FILE__).'/legacy/OmiseToken.php';
+require_once dirname(__FILE__).'/legacy/OmiseCharge.php';
+require_once dirname(__FILE__).'/legacy/OmiseCustomer.php';
+require_once dirname(__FILE__).'/legacy/OmiseRefund.php';
+require_once dirname(__FILE__).'/legacy/OmiseRefundList.php';
+require_once dirname(__FILE__).'/legacy/OmiseTransfer.php';
+require_once dirname(__FILE__).'/legacy/OmiseTransaction.php';
+require_once dirname(__FILE__).'/legacy/OmiseRecipient.php';
