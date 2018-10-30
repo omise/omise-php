eefd746af200a015e6ef4c9f0cdd9c9f667614cf
Jon Randy <jonrandy@gmail.com>
2018-10-30 16:23:39 +0700

    Adds methods to facilitate getting information on payment backends from capabilities data

--- lib/omise/OmiseCapabilities.php
+++ lib/omise/OmiseCapabilities.php
@@ -18,6 +18,53 @@ class OmiseCapabilities extends OmiseApiResource
     }
 
     /**
+     * Retrieves array of payment backends.
+     *    
+     * @return array
+     */
+    public function getBackends() {
+        // check for filters
+        if ($filters = func_get_args()) $filter = self::combineFilters($filters);
+        $res = $this['payment_backends'];
+        array_walk(
+            $res,
+            function($v, $k) use (&$res) {
+                $id = array_keys($v)[0];
+                $res[$k][$id]['_id'] = $id;
+            }
+        );
+        $res = array_map(function($a) { return (object)reset($a);}, $res);
+        return $filter ? array_filter($res, $filter) : $res;
+    }
+
+
+    public static function backendSupportsCurrency($currency) {
+        return function($backend) use ($currency) { return in_array($currency, $backend->currencies); };
+    }
+
+    public static function backendTypeIs($type) {
+        return function($backend) use ($type) { return $backend->type==$type; };
+    }
+
+    public static function backendSupportsAmount($amount) {
+        return function($backend) use ($amount) { return !empty($backend->amount) && $backend->amount['min']<=$amount && $backend->amount['max']>=$amount; };
+    }
+
+
+    /**
+     * Combines boolean filters.
+     *    
+     * @return function
+     */
+    public static function combineFilters($filters) {
+        return function($a) use ($filters) {
+            $res = true;
+            foreach ($filters as $filter) $res &= $filter($a);
+            return $res;
+        };
+    }
+
+    /**
      * (non-PHPdoc)
      *
      * @see OmiseApiResource::g_reload()
