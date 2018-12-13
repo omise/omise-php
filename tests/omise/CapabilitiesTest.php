60ec64dfc6f086b7dc947647169e0e381effec4c
Jacek <jacek.stanusz@gmail.com>
2018-12-13 10:38:25 +0700

    Capabilities testing - added reload function test

--- tests/omise/CapabilitiesTest.php
+++ tests/omise/CapabilitiesTest.php
@@ -2,19 +2,33 @@
 
 class CapabilitiesTest extends TestConfig {
   /**
-   * OmiseAccount class must be contain some method below.
+   * OmiseCapabilities class must be contain some method below. 
    *
    */
   public function testMethodExists() {
     $this->assertTrue(method_exists('OmiseCapabilities', 'retrieve'));
+    $this->assertTrue(method_exists('OmiseCapabilities', 'reload'));
   }
 
   /**
-   * Assert that an account object is returned after a successful retrieve.
+   * Assert that a capabilities object is returned after a successful retrieve.
    *
    */
   public function testRetrieveOmiseCapabilitiesObject() {
     $capabilities = OmiseCapabilities::retrieve();
+
+    $this->assertArrayHasKey('payment_backends', $capabilities);
+    $this->assertInternalType('array', $capabilities['payment_backends']);
+  }
+
+  
+  /**
+   * Assert that a capabilities object is returned after a successful reload.
+   *
+   */
+  public function testReload() {
+    $capabilities = OmiseCapabilities::retrieve();
+    $capabilities->reload();
 
     $this->assertArrayHasKey('payment_backends', $capabilities);
     $this->assertInternalType('array', $capabilities['payment_backends']);
