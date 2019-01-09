2059b15a16a9a9550188815e940b52f6993c2006
guzzilar <iam.nuttanon@gmail.com>
2019-01-09 16:26:33 +0700

    Bump release number to v2.11.0. Update CHANGELOG.

--- lib/omise/res/OmiseApiResource.php
+++ lib/omise/res/OmiseApiResource.php
@@ -1,6 +1,6 @@
 <?php
 
-define('OMISE_PHP_LIB_VERSION', '2.10.0');
+define('OMISE_PHP_LIB_VERSION', '2.11.0');
 define('OMISE_API_URL', 'https://api.omise.co/');
 define('OMISE_VAULT_URL', 'https://vault.omise.co/');
 
