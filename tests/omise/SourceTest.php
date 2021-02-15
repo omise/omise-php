601444d8284c9eab970f173b2314691ca3ec6e71
Keerati <keerati.tansawatcharoen@gmail.com>
2021-02-15 09:08:34 +0700

    add test for OmiseSource::retrieve

--- tests/omise/SourceTest.php
+++ tests/omise/SourceTest.php
@@ -3,6 +3,17 @@ require_once dirname(__FILE__).'/TestConfig.php';
 
 class SourceTest extends TestConfig
 {
+
+     /**
+     * @test
+     * OmiseSource class must be contain some method below.
+     */
+    public function method_exists()
+    {
+        $this->assertTrue(method_exists('OmiseSource', 'retrieve'));
+        $this->assertTrue(method_exists('OmiseSource', 'create'));
+    }
+
     /**
      * @test
      */
@@ -15,6 +26,17 @@ class SourceTest extends TestConfig
         );
 
         $source = OmiseSource::create($parameter);
+
+        $this->assertArrayHasKey('object', $source);
+        $this->assertEquals('source', $source['object']);
+    }
+
+     /**
+     * @test
+     */
+    public function retrieve_specifics_source_object()
+    {
+        $source = OmiseSource::retrieve('src_test_no1t4tnemucod0e51mo');
 
         $this->assertArrayHasKey('object', $source);
         $this->assertEquals('source', $source['object']);
