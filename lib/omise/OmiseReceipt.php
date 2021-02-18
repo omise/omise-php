56606e7b50429850fe8f14743c6ea7cdc5167f6f
Chanapa <chanapa.pntt@hotmail.com>
2021-02-18 11:25:47 +0700

    Add Receipt API (#126)

---
+++ lib/omise/OmiseReceipt.php
@@ -0,0 +1,46 @@
+<?php
+
+class OmiseReceipt extends OmiseApiResource
+{
+    const ENDPOINT = 'receipts';
+
+    /**
+     * Retrieves a receipt.
+     *
+     * @param  string $id
+     * @param  string $publickey
+     * @param  string $secretkey
+     *
+     * @return OmiseReceipt
+     */
+    public static function retrieve($id = '', $publickey = null, $secretkey = null)
+    {
+        return parent::g_retrieve(get_class(), self::getUrl($id), $publickey, $secretkey);
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see OmiseApiResource::g_reload()
+     */
+    public function reload()
+    {
+        if ($this['object'] === 'event') {
+            parent::g_reload(self::getUrl($this['id']));
+        } else {
+            parent::g_reload(self::getUrl());
+        }
+    }
+
+    /**
+     * Generate request url.
+     *
+     * @param  string $id
+     *
+     * @return string
+     */
+    private static function getUrl($id = '')
+    {
+        return OMISE_API_URL.self::ENDPOINT.'/'.$id;
+    }
+}
