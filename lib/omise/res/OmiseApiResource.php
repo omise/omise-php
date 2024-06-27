46d77324e2ab530113ca676a16456c3d1325e1a0
Aashish <aashish@omise.co>
2024-06-27 17:52:49 +0700

    Add #[AllowDynamicProperties] to not emit deprecation notice.

--- lib/omise/res/OmiseApiResource.php
+++ lib/omise/res/OmiseApiResource.php
@@ -4,6 +4,7 @@ define('OMISE_PHP_LIB_VERSION', '2.17.0');
 define('OMISE_API_URL', 'https://api.omise.co/');
 define('OMISE_VAULT_URL', 'https://vault.omise.co/');
 
+#[AllowDynamicProperties]
 class OmiseApiResource extends OmiseObject
 {
     // Request methods
