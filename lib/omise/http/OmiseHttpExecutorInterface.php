d320e6205474fdb11447182ba6cfa3bb3912c261
Rossukhon Leagmongkol <rossukhon.l@opn.ooo>
2025-03-27 17:06:18 +0700

    [MIT-3253] Fix space format

--- lib/omise/http/OmiseHttpExecutorInterface.php
+++ lib/omise/http/OmiseHttpExecutorInterface.php
@@ -1,15 +1,16 @@
 <?php
 
-interface OmiseHttpExecutorInterface {
-  /**
-   * @param  string $url
-   * @param  string $requestMethod
-   * @param  string $key
-   * @param  array  $params
-   *
-   * @throws OmiseException
-   *
-   * @return string
-   */
-  public function execute($url, $requestMethod, $key, $params = null);
+interface OmiseHttpExecutorInterface
+{
+    /**
+     * @param  string $url
+     * @param  string $requestMethod
+     * @param  string $key
+     * @param  array  $params
+     *
+     * @throws OmiseException
+     *
+     * @return string
+     */
+    public function execute($url, $requestMethod, $key, $params = null);
 }
